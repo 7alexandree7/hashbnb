8fdbf363f233be8a9c875d4e1178f312db3be4cc
alexandre <alexandre020602@gmail.com>
2025-04-19 00:39:01 -0300

    criando o componente de Items com dados mackados, e resolvendo alguns aspectos de responsividade no componente Item e no Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './Components/Header/Header'
+import Item from './Components/Item/Item'
 
 
 function App() {
@@ -7,6 +8,18 @@ function App() {
   return (
     <>
       <Header />
+      <section>
+        <div className='mx-auto max-w-7xl p-8 grid grid-cols-[repeat(auto-fit,minmax(225px,1fr))] gap-8'>
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+        </div>
+      </section>
     </>
   )
 }
--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -6,7 +6,7 @@ import UserAvatarIcon from "../Icons/UserAvatarIcon/UserAvatarIcon";
 const Header = () => {
   return (
     <div className="shadow-md">
-      <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-4">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-4 sm:px-8">
         <div className="flex items-center">
           <img
             src="https://cdn.prod.website-files.com/61b9e0dd381626819c8d4f83/65e2198d48039ba6444f602b_logo%20hashtag%20-%20h.webp"
@@ -16,7 +16,7 @@ const Header = () => {
           <p className="text-2xl font-bold text-primary-400">ashbnb</p>
         </div>
 
-        <div className="flex items-center border border-gray-300 px-4 py-2 pl-6 rounded-full">
+        <div className=" hidden lg:flex items-center border border-gray-300 px-4 py-2 pl-6 rounded-full">
           <p className="border-r border-r-gray-300 pr-4">Any where</p>
           <p className="border-r border-r-gray-300 px-4">Any Week</p>
           <p className="px-4">Guests</p>
@@ -28,7 +28,7 @@ const Header = () => {
             <UserAvatarIcon />
             <MenuIcon />
           </div>
-          <p className="">Alexandre Oliveira</p>
+          <p className="sm:max-w-40 max-w-20 truncate">Alexandre Oliveira</p>
         </div>
         
       </div>
