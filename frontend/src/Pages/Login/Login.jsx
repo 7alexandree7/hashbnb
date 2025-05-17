e4753bfdeb0fb7b20de119de04abc32660d0332a
alexandre <alexandre020602@gmail.com>
2025-05-16 21:04:58 -0300

    criando, um roadmap .txt do raciocinio para um login,  e criando a função de submit para o login, com as variaveis de estado

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -1,25 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom";
 
 const Login = () => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <>
       <section className='flex items-center'>
         <div className='mx-auto w-full max-w-96 gap-4 flex flex-col items-center'>
           <h1 className='text-3xl font-bold'>Faça seu login</h1>
 
-          <form className='flex flex-col w-full gap-2'>
+          <form className='flex flex-col w-full gap-2' onSubmit={handleSubmit}>
             <input
               type="email"
+              required
               className='border border-gray-300 rounded-full px-4 py-2'
               placeholder='examplo@exemplo.com'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
 
             <input
               type="password"
+              required
               autoComplete='on'
               className='border border-gray-300 rounded-full px-4 py-2'
               placeholder='********'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
 
             <button className='cursor-pointer bg-primary-400 rounded-full px-4 py-2 text-white font-bold'>Login</button>
