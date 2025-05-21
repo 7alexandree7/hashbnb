f010bc26928fe13ff843271280d8deee80d10c47
alexandre <alexandre020602@gmail.com>
2025-05-21 10:37:23 -0300

    add mid cors

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,16 @@
 import "dotenv/config";
 import express from "express";
 import UserRoutes from "./router/users/routes.js";
-
-
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
+
 app.use('/users', UserRoutes)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000
 
 
 app.listen(PORT, () => {
