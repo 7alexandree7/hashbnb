be006cab8f2b3638ad94362092fc990146cb75a8
alexandre <alexandre020602@gmail.com>
2025-05-22 21:03:51 -0300

    adicionando nova rota de register e passando propiedades para os childrens

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,24 @@
+import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
 import Home from './Pages/Home/Home'
 import Login from './Pages/Login/Login'
+import Register from './Pages/Register/Register'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 
 function App() {
 
+  const [user, setUser] = useState(null)
+
   return (
     <>
       <BrowserRouter>
-        <Header />
+        <Header user={user} />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
+          <Route path='/login' element={<Login user={user} setUser={setUser} />} />
+          <Route path='/register' element={<Register setUser={setUser} />} />
         </Routes>
       </BrowserRouter>
     </>
