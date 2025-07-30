793d8c6f063ed78bb1d414cc6f9d1cf6792ee446
alexandre <alexandre020602@gmail.com>
2025-07-30 20:20:37 -0300

    mudando o type dos inputs em Login

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -11,12 +11,12 @@ const Login = () => {
                     <input
                         className='w-full outline-none rounded-full px-4 py-2 border border-gray-300'
                         placeholder='example@gmail.com'
-                        type="text"
+                        type="email"
                         required />
                     <input
                         className='w-full outline-none rounded-full px-4 py-2 border border-gray-300'
                         placeholder='********'
-                        type="text"
+                        type="password"
                         required />
                     <button
                         className='bg-primary-400 text-white rounded-full px-4 py-2 cursor-pointer'>
